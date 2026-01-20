5f7ffd959d7066caedc66fd096848cee9b6b4306
Sam Mumford <smmmfrd@gmail.com>
2026-01-20 00:05:42 -0800

    Separate collected text and amount.
    With the count updating and our font not being monospace, the UI would keeping jumping around whenever the amount was updated. So they were separated.

--- gameplay_ui.gd
+++ gameplay_ui.gd
@@ -2,9 +2,9 @@ class_name GameplayUI
 extends Control
 
 @onready var level_name_text: RichTextLabel = %"Level Name Text"
-@onready var collected_text: RichTextLabel = %"Collected Text"
+@onready var amount_text: RichTextLabel = %"Amount Text"
 
-var collect_text = "Collected: %d/%d"
+var collect_text = "%d/%d"
 var max_collects := 0
 
 func level_stats(level_name: String, collect_count: int) -> void:
@@ -14,4 +14,4 @@ func level_stats(level_name: String, collect_count: int) -> void:
 	collected_amount_update(0, false)
 
 func collected_amount_update(amount: int, _complete:bool) -> void:
-	collected_text.text = collect_text % [amount, max_collects]
+	amount_text.text = collect_text % [amount, max_collects]
