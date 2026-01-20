84bf3141c505532531fc7b4cd3e954853bb10c31
Sam Mumford <smmmfrd@gmail.com>
2026-01-20 01:16:46 -0800

    Picking collect up UI juice and Refactored UI
    Put all UI stuff into it's own folder. The text effect on the collect pick up is as much as I'm going to do.

--- UI/gameplay_ui.gd
+++ UI/gameplay_ui.gd
@@ -4,14 +4,23 @@ extends Control
 @onready var level_name_text: RichTextLabel = %"Level Name Text"
 @onready var amount_text: RichTextLabel = %"Amount Text"
 
+var effect_duration = 0.25
+
+
 var collect_text = "%d/%d"
+var animated_collect_text = "[greentint][wave amp=-36 freq=10]%d[/wave][/greentint]/%d"
 var max_collects := 0
+
 
 func level_stats(level_name: String, collect_count: int) -> void:
 	level_name_text.text = level_name
 	max_collects = collect_count
 	
-	collected_amount_update(0, false)
+	amount_text.text = collect_text % [0, max_collects]
 
 func collected_amount_update(amount: int, _complete:bool) -> void:
+	amount_text.text = animated_collect_text % [amount, max_collects]
+	
+	await get_tree().create_timer(effect_duration).timeout
+	
 	amount_text.text = collect_text % [amount, max_collects]
